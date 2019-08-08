1848a2a2d5621dc1f0365365ea109445888d9e11
Zach Resnick <zachary.l.resnick@gmail.com>
2019-08-08 11:17:09 -0400

    Restarting Project

--- server/db/index.js
+++
@@ -1,9 +0,0 @@
-// The purpose of this module is to bring your Sequelize instance (`db`) together
-// with your models (which you should define in separate modules in this directory).
-
-const db = require('./db')
-
-// register models
-require('./models')
-
-module.exports = db
